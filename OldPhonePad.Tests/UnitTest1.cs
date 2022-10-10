9c0197bfdde29be468c670bd229c4f27c9bd6809
benjamin <b.james.neustadt@gmail.com>
2022-10-10 10:29:43 +0200

    Setup Project

---
+++ OldPhonePad.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace OldPhonePad.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ OldPhonePad/OldPhonePad.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OldPhonePad.Translate
+{
+    public class OldPhonePad
+    {
+        public bool Translate(string input)
+        {
+            // Implement code here
+        }
+    }
+}
