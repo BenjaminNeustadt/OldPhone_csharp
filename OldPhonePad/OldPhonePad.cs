928fae33ac407161f4feb69b401fe4f83fe496c8
benjamin <b.james.neustadt@gmail.com>
2022-10-10 11:53:25 +0200

    Add a Lexicon

--- OldPhonePad/OldPhonePad.cs
+++ OldPhonePad/OldPhonePad.cs
@@ -6,6 +6,7 @@ namespace OldPhone.Translate
     {
         public string Translate(string input)
         {
+
           if (input == "2")
           {
             return "A";
@@ -16,5 +17,40 @@ namespace OldPhone.Translate
           }
           return "B";
         }
+
+        public Lexicon<string, string>  ValueLookup = new Lexicon<string, string>
+        {
+          [ "1" ]   =  "&",
+          ["11"]   =  ",",
+          ["111"]  =  "(",
+          ["0"]    =  " ",
+          ["2"]    =  "A",
+          ["22"]   =  "B",
+          ["222"]  =  "C",
+          ["3"]    =  "D",
+          ["33"]   =  "E",
+          ["333"]  =  "F",
+          ["4"]    =  "G",
+          ["44"]   =  "H",
+          ["444"]  =  "I",
+          ["5"]    =  "J",
+          ["55"]   =  "K",
+          ["555"]  =  "L",
+          ["6"]    =  "M",
+          ["66"]   =  "N",
+          ["666"]  =  "O",
+          ["7"]    =  "P",
+          ["77"]   =  "Q",
+          ["777"]  =  "R",
+          ["7777"] =  "S",
+          ["8"]    =  "T",
+          ["88"]   =  "U",
+          ["888"]  =  "V",
+          ["9"]    =  "W",
+          ["99"]   =  "X",
+          ["999"]  =  "Y",
+          ["9999"] =  "Z"
+        };
+
     }
 }
